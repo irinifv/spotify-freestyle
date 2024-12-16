0a0083363d9d3938e67961ed7569cce4e0d2caff
Jackson Konstance <143450706+jrk156@users.noreply.github.com>
2024-12-15 22:45:14 -0500

    fixed flask, integrated .gitignore
    fixed flask issue where naming of templates was incorrect
    revised .gitignore file
    added FLASK instructions in README

--- app/main2.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-
-# Create the Flask app using the factory function
-app = create_app()
-
-if __name__ == "__main__":
-    # Run the app in debug mode
-    app.run(debug=True)
